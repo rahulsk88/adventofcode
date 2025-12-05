53bc29b6bd9f5b28f9418cb568c6a42c757a0b1a
Rahul <80764087+rahulsk88@users.noreply.github.com>
2025-12-05 00:08:26 -0500

    finished part 2

--- day3_part2.py
+++ day3_part2.py
@@ -18,11 +18,15 @@ def get_max_joltage(content):
         except:
             battery_index = 0
             start_index = 0
+        battery_value = 0
 
         while start_index < len(bank) - (12 - len(bank_index)) + 1:  # What if you are at the last. 
                                                               # Then it should automatically stop
-            if int(bank[start_index]) > int(bank[battery_index]):
+            
+            current_value = int(bank[start_index]) 
+            if current_value > battery_value:
                 battery_index = start_index
+                battery_value = int(bank[start_index])
         
             start_index += 1
 
@@ -34,36 +38,20 @@ def get_max_joltage(content):
 
 
             
-    
+    print(banks)
+
     for bank in banks:
         bank_index = [] # Has to be exactly 12 long
         
         while len(bank_index) < 12:
             _bank_max_finder(bank, bank_index)
-
-
-        # battery_index = 0
-        # while battery_index < len(bank)-1:
-        #     if int(bank[battery_index]) > int(bank[high_index]):
-        #         high_index = battery_index
-            
-        #     battery_index += 1
-
-        # battery_index = high_index + 1
-        # second_high_index = high_index + 1
         
-        # while battery_index <= len(bank)-1:
-        #     if int(bank[battery_index]) > int(bank[second_high_index]):
-        #         second_high_index = battery_index
-        #     battery_index += 1
-
-        # print(high_index, second_high_index)
-        # assert high_index < second_high_index
-       
-        # string_joltage =  bank[high_index] + bank[second_high_index]
-        # max_joltage += int(string_joltage)
-
+        print(bank_index)
+        
+        max_joltage += int("".join([bank[i] for i in bank_index]))
+    
     return max_joltage
+
 
 print(get_max_joltage(content))
         
